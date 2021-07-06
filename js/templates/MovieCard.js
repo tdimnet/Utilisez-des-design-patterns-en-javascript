de267c71d90742dbce96164d6e264e11f415786a
Thomas Dimnet <thomas.dimnet@gmail.com>
2021-07-06 09:44:10 +0200

    Work on the design of the card

--- js/templates/MovieCard.js
+++ js/templates/MovieCard.js
@@ -5,15 +5,21 @@ class MovieCard {
 
     createMovieCard() {
         const $wrapper = document.createElement('div')
+        $wrapper.classList.add('movie-card-wrapper')
 
         const movieCard = `
-            <h3>${this._movie.title}</h3>
-            <div class="movie-thumbnail">
+            <div class="movie-thumbnail center">
                 <img
                     alt="${this._movie.title}"
                     src="/assets/thumbnails/${this._movie.picture}"
                 />
             </div>
+            <h3 class="fs-16 center">${this._movie.title}</h3>
+            <p class="fs-14 center">
+                <span>${this._movie.released_in}</span>
+                -
+                <span>${this._movie.duration}</span>
+            </p>
         `
         $wrapper.innerHTML = movieCard
         return $wrapper
