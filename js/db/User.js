d76ed6750c8d8b61e2c374a92de81d70c76e08b5
Thomas Dimnet <thomas.dimnet@gmail.com>
2021-07-08 17:05:17 +0200

    Add comments to help students understanding the Singleton

--- js/db/User.js
+++ js/db/User.js
@@ -3,11 +3,16 @@ class User {
         if (User.exists) {
             return User.instance
         } else if (data && data.firstName && data.lastName) {
+            // Si firstName et lastName sont définis au moment de l'instanciation
+
+            // J'initialise les propriétés firstName et lastName
             this._firstName = data.firstName
             this._lastName = data.lastName
 
+            // Je les sauvegarde en Local Storage
             this.saveToLocalStorage()
 
+            // Je "lock" l'objet
             User.instance = this
             User.exists = true
             return this
@@ -23,9 +28,11 @@ class User {
     }
 
     get user() {
+        // Vérifie si firstName et lastName existent soit au sein de la classe, soit en LocalStorage
         const firstName = this._firstName || localStorage.getItem('firstName')
         const lastName = this._lastName || localStorage.getItem('lastName')
 
+        // Si oui, alors je réinstancie : on a besoin de ce bout de code une fois que l'application a été quittée.
         if (firstName && lastName) {
             const user = new User({
                 firstName,
@@ -33,10 +40,12 @@ class User {
             })
         }
 
+        // Sinon, ça veut dire que la classe n'a pas été instancié.
         if (!firstName && !lastName) {
             return null
         }
 
+        // Ici, je retourne le user
         return {
             firstName: firstName,
             lastName: lastName,
