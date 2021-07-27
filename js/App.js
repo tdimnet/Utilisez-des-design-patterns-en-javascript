178012fb28488c7b7a30b0942fcc88b1f6dd49b5
Thomas Dimnet <thomas.dimnet@gmail.com>
2021-07-27 10:42:06 +0200

    ajout de la question 3 de la partie 2 du quiz

--- js/App.js
+++ js/App.js
@@ -1,18 +1,17 @@
+const fetchData = () => fetch('/data/data.json')
+    .then(res => res.json())
+    .catch(e => console.log('error', e))
+
+
+class Movie {
+}
+
+
 class App {
-    constructor() {
-        this.$moviesWrapper = document.querySelector('.movies-wrapper')
-        this.moviesApi = new MovieApi('/data/movie-data.json')
-    }
-
-    async main() {
-        const movies = await this.moviesApi.getMovies()
-
-        movies.forEach(movie => {
-            const Template = new MovieCard(movie)
-            this.$moviesWrapper.appendChild(Template.createMovieCard())        
-        })    
+    async init() {
+        const data = await fetchData()
     }
 }
 
 const app = new App()
-app.main()
+app.init()
